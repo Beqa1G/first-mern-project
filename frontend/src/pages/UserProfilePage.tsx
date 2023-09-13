d9b116027e45ae805900ba23e6e4dca6d316dd7b
Beqa1G <beqa.gurgenidze.2013@gmail.com>
2023-09-13 20:05:13 +0400

    applied few debugs

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -16,7 +16,7 @@ export default function UserProfilePage() {
     if (!username) {
       throw new Error("Username is missing");
     }
-    const response = await fetchData("api/users/" + username, {
+    const response = await fetchData("api/userprofile/" + username, {
       method: "GET",
       credentials: "include",
     });
@@ -25,38 +25,54 @@ export default function UserProfilePage() {
       throw new Error("Could not fetch Data!");
     }
     const responseData = await response.json();
-    console.log("Fetched Data:", responseData); // Add this line
+    /* console.log("Fetched Data:", responseData); // Add this line */
     return responseData;
+  },
+  {
+    retry: false, // Disable automatic retries
+    staleTime: 0, // Do not use cached data
   });
+  
+  if (User) {
+    console.log("user profile: ", User);
+  }
 
-  console.log("user profile: ", User);
+
+ 
+  if(isLoading) {
+    return <Spinner className={styles.loadingStates} animation="border" />
+  } 
+
+  if(isError || !User) {
+    return (<><h2>Sorry, such user doesn't exists</h2> {console.log(User)}</>)
+  }
+
+
 
   return (
-    <div>
-      {isLoading && (
-        <Spinner className={styles.loadingStates} animation="border" />
-      )}
-      {isError && <div>Error fetching user profile</div>}
-      {User && (
-        <div className={styles.profileCard}>
-          <h2>User Profile</h2>
-          <p className={styles.usernameP}>{User?.user.username}</p>
-          <div className={styles.profileLinkCardWrapper}>
-            {User?.links.map((link) => (
-              <div key={link?._id} className={styles.profileLinkCard}>
-                <a
-                  className={`${styles.linkUrl} linkUrl`}
-                  href={link.Url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {link.Url}
-                </a>
-              </div>
-            ))}
-          </div>
+      <div className={styles.profileCard}>
+        <h2>User Profile</h2>
+        <p className={styles.usernameP}>{User?.user.username}</p>
+        <div className={styles.profileLinkCardWrapper}>
+        {User?.links.length === 0 ? (
+          <h2 className={styles.noLinksMessage}>
+            User doesn't have links yet.
+          </h2>
+        ) : (
+          User?.links.map((link) => (
+            <div key={link?._id} className={styles.profileLinkCard}>
+              <a
+                className={`${styles.linkUrl} linkUrl`}
+                href={link.Url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {link.Url}
+              </a>
+            </div>
+          ))
+        )}
         </div>
-      )}
-    </div>
+      </div>
   );
 }
